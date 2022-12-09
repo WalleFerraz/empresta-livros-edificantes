571b295c426c20353cde691bd44fe93dd72ee4d9
Walisson Ferraz <walissonrodriferraz@gmail.com>
2022-12-09 10:43:45 -0400

    Create home.php
    Header e footer

---
+++ pages/home.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Empresta Livros</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+
+<body>
+    <nav>
+        <div class="nav-wrapper">
+
+            <a href="../pages/home.php">
+                <img src="../images/logo-empresta-livro.png" alt="Logo do site empresta livro" class="logoSize">
+            </a>
+
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Livros</a></li>
+                <li><a href="#">Leitores</a></li>
+                <li><a href="#">Controle</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    <footer class="page-footer">
+        <div class="footer-copyright">
+            <div class="container">
+                © 2014 Copyright Empresta Livro
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>

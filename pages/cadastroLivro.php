efdf865eca170e2c969f9443201d22609b430d0c
Walisson Ferraz <walissonrodriferraz@gmail.com>
2022-12-09 15:02:41 -0400

    Criação da tela de cadastro de livro
    Criação da página e do formulário de cadastro de um novo livro

---
+++ pages/cadastroLivro.php
@@ -0,0 +1,109 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de um novo livro</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+    <link href="../js/document.js">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+
+</head>
+
+<body>
+    <nav>
+        <div class="nav-wrapper">
+
+            <a href="../pages/home.php">
+                <img src="../images/logo-empresta-livro.png" alt="Logo do site empresta livro" class="logoSize">
+            </a>
+
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Livros</a></li>
+                <li><a href="#">Leitores</a></li>
+                <li><a href="#">Controle</a></li>
+                <li><a href="cadastroLeitor.php">Cadastrar Leitor</a></li>
+                <li><a href="cadastroLivro.php">Cadastrar Livro</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Cadastro de livro -->
+    <div class="center middle-box">
+        <h4>
+            Cadastrar um novo livro
+        </h4>
+        <div class="row">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input id="book_name" type="text" class="validate">
+                        <label for="book_name">Título do livro</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input id="author_name" type="text" class="validate">
+                        <label for="author_name">Autor(a) do livro</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s5">
+                        <select>
+                            <option value="" disabled selected>Categoria</option>
+                            <option value="1">Meditação</option>
+                            <option value="2">História</option>
+                            <option value="3">Estudo</option>
+                            <option value="4">Conhecimento da Igreja</option>
+                            <option value="5">Biografia</option>
+                        </select>
+                    </div>
+                    <div class="input-field col s5">
+                        <select>
+                            <option value="" disabled selected>Público indicado</option>
+                            <option value="1">Criança</option>
+                            <option value="2">Jovem</option>
+                            <option value="3">Adulto</option>
+                            <option value="4">Namorados</option>
+                            <option value="5">Casados</option>
+                        </select>
+                    </div>
+                    <div class="input-field col s2">
+                        <input id="number_pages" type="number" class="validate">
+                        <label for="number_pages">Páginas</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <textarea id="textarea" class="materialize-textarea"></textarea>
+                        <label for="textarea">Descrição do livro...</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <button class="btn waves-effect waves-light red accent-2" type="submit" name="action">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+    <footer class="page-footer">
+        <div class="footer-copyright">
+            <div class="container">
+                © 2022 Copyright Empresta Livro
+            </div>
+        </div>
+    </footer>
+
+    <script>
+        M.AutoInit();
+        $(document).ready(function() {
+            $('select').formSelect()
+        });
+    </script>
+</body>
+
+</html>

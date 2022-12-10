5eb917fcac70aa362f641de67a69842fc46385de
Walisson Ferraz <walissonrodriferraz@gmail.com>
2022-12-10 14:48:12 -0400

    Update consultBookName.php

--- function/consultBookName.php
+++ function/consultBookName.php
@@ -3,8 +3,8 @@ require 'connection.php';
 
 function consultBookName($nome){
     $pdo = connection();
-    $stmt = $pdo -> prepare('SELECT * FROM livro WHERE nomeLivro LIKE :nome%'); 
-    $stmt -> bindValue(':nome', $nome);
+    $stmt = $pdo -> prepare('SELECT * FROM livro WHERE nomeLivro LIKE :nome'); 
+    $stmt -> bindValue(':nome', $nome.'%');
     $stmt -> execute();
     $pdo = null;
     return $stmt;

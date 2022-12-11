7a46b32d4f2ec17b64239df045b391ebfdb7d822
Walisson Ferraz <walissonrodriferraz@gmail.com>
2022-12-11 18:42:11 -0400

    Update consultReaderName.php

--- function/consultReaderName.php
+++ function/consultReaderName.php
@@ -3,8 +3,8 @@ require 'connection.php';
 
 function consultReaderName($nome){
     $pdo = connection();
-    $stmt = $pdo -> prepare('SELECT * FROM livro WHERE nomeLeitor LIKE :nome%'); 
-    $stmt -> bindValue(':nome', $nome);
+    $stmt = $pdo -> prepare('SELECT * FROM leitor WHERE nomeLeitor LIKE :nome'); 
+    $stmt -> bindValue(':nome', $nome.'%');
     $stmt -> execute();
     $pdo = null;
     return $stmt;

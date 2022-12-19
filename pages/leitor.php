c06313bd2cc8dd3c8caa130ae2b58545e07ef358
Walisson Rodrigues Ferraz <walissonrodriferraz@gmail.com>
2022-12-19 12:13:35 -0400

    Update leitor.php

--- pages/leitor.php
+++ pages/leitor.php
@@ -38,6 +38,7 @@ if (!empty($_GET)) {
                 <li><a href="leitor.php">Leitores</a></li>
                 <li><a href="cadastroLeitor.php">Cadastrar Leitor</a></li>
                 <li><a href="cadastroLivro.php">Cadastrar Livro</a></li>
+                <li><a href="cadastroEmprestimo.php">Cadastrar Empréstimo</a></li>
             </ul>
         </div>
     </nav>

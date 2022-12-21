68f98710b905fdd912212b0fd5420c12c5161e7e
Walisson Rodrigues Ferraz <walissonrodriferraz@gmail.com>
2022-12-21 12:18:36 -0400

    Update insertLoan.php

--- function/insertLoan.php
+++ function/insertLoan.php
@@ -3,7 +3,7 @@ require 'connection.php';
 
 function insertLoan($idLivro, $idLeitor, $devolucao){
     $pdo = connection();
-    $stmt = $pdo -> prepare('INSERT INTO emprestimo VALUES(DEFAULT, :livro, :leitor, NOW(), :devolucao)'); 
+    $stmt = $pdo -> prepare('INSERT INTO emprestimo VALUES(DEFAULT, :livro, :leitor, DEFAULT, :devolucao)'); 
     $stmt -> bindValue(':livro', $idLivro);
     $stmt -> bindValue(':leitor', $idLeitor);
     $stmt -> bindValue(':devolucao', $devolucao);
@@ -11,4 +11,14 @@ function insertLoan($idLivro, $idLeitor, $devolucao){
     $pdo = null;
     return $stmt;
 }//insertLoan
+
+function updateBookEstado($id, $estado){
+    $pdo = connection();
+    $stmt = $pdo -> prepare('UPDATE livro SET estado = :estado WHERE id = :id'); 
+    $stmt -> bindValue(':estado', $estado);
+    $stmt -> bindValue(':id', $id);
+    $stmt -> execute();
+    $pdo = null;
+    return $stmt;
+}//updateBookEstado
 ?>
